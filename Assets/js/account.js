29e5e8739ba39cc23884a1a4acec587ac1887b3e
hà <4301104117@student.hcmup.edu.vn>
2019-05-27 17:53:01 +0700

    Added necessary files

---
+++ Assets/js/account.js
@@ -0,0 +1,44 @@
+var accounts = [
+	{
+		id: 1,
+		username: "duynm619",
+		password: "123123123",
+		role: "admin",
+		status: false,
+		img : "image/ava.png",
+		// infomation
+		name: "Nguyễn Minh Duy",
+		gender: "Nam",
+		state: "Vietnam",
+		email: "minhduynguyen10@gmail.com",
+		phone: "0933047332",
+		born: "06/11/1999",
+		card: "95513531212",
+
+		wishlist: [1,22,13]
+
+	},
+	{
+		id: 2,
+		username: "minhduy",
+		password: "123123123",
+		role: "customer",
+		status: false,
+		img : "image/poro.jpg",
+		// infomation
+		name: "Nguyễn Minh Duy",
+		gender: "Nam",
+		state: "Vietnam",
+		email: "minhduynguyen10@gmail.com",
+		phone: "0933047332",
+		born: "16/4/1999",
+		card: "3531131212",
+
+		wishlist: [5,6,9]
+
+
+	},
+		
+
+		
+];
